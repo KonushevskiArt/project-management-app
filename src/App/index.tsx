5bf68bec5b670e79d1673efe6298313342a411b8
Artur Mingazhov <arthurming7@gmail.com>
2022-05-10 19:52:51 +0300

    feat: add global styles

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import './App.scss';
+import './global-styles.scss';
 import AppRoutes from './AppRoutes';
 
 function App() {
